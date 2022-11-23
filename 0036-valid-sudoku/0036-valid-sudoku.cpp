3af6d235c3ddc70726db4d806f11f5c1b363950a
NALIN AGRAWAL <54065357+nalin-programmer@users.noreply.github.com>
2022-11-23 22:56:01 +0530

    Time: 52 ms (40.47%), Space: 19.3 MB (49.63%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        for(int i=0;i<9;++i){
+        map<char,int> mpp;
+        for(int j=0;j<9;++j){
+            if(board[i][j]!='.'){
+                mpp[board[i][j]]++;
+                if(mpp[board[i][j]]>1){
+                    return false;
+                }
+            }
+        }
+    }
+    for(int i=0;i<9;++i){
+        map<char,int> mpp;
+        for(int j=0;j<9;++j){
+            if(board[j][i]!='.'){
+                mpp[board[j][i]]++;
+                if(mpp[board[j][i]]>1){
+                    return false;
+                }
+            }
+        }
+    }
+    for(int k=0;k<9;k+=3){
+        map<char,int> mpp1;
+        for(int i=k;i<k+3;++i){
+            for(int j=0;j<3;++j){
+                if(board[i][j]!='.'){
+                    mpp1[board[i][j]]++;
+                    if(mpp1[board[i][j]]>1){
+                        return false;
+                    }
+                }
+            }
+        }
+        map<char,int> mpp2;
+        for(int i=k;i<k+3;++i){
+            for(int j=3;j<6;++j){
+                if(board[i][j]!='.'){
+                    mpp2[board[i][j]]++;
+                    if(mpp2[board[i][j]]>1){
+                        return false;
+                    }
+                }
+            }
+        }
+        map<char,int> mpp3;
+        for(int i=k;i<k+3;++i){
+            for(int j=6;j<9;++j){
+                if(board[i][j]!='.'){
+                    mpp3[board[i][j]]++;
+                    if(mpp3[board[i][j]]>1){
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+    return true; 
+    }
+};
